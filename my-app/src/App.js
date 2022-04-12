e64bb316681594bd787dd0c890c6569788c2b422
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-12 15:09:55 +0100

    setting up app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import * as Pages from '../Pages'
+import { Routes, Route } from 'react-router-dom';
+import { NavBar } from '../components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+        <main>
+          <Routes>
+              <Route path="/" element={<Pages.Home />}>
+                <Route path=":username" element={<Pages.Repos />}>
+                </Route>
+              </Route>
+          </Routes>
+        </main>
     </div>
   );
 }
