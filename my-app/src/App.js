3f514520326526905d73cbff74ca8739532f7c77
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-12 15:36:37 +0100

    adding github api to searchbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import * as Pages from './Pages'
+import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components'
 import './App.css';
 
 function App() {
+  const userFetch = async (username) => {
+    try {
+      const { data } = await axios.get(`https://api.github.com/users/${username}/repos`)
+      console.log(data)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar getUser={userFetch}/>
         <main>
           <Routes>
               <Route path="/" element={<Pages.Home />}>
--- my-app/src/components/NavBar/index.jsx
+++ my-app/src/components/NavBar/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({getUser}) => {
+
+    const [ username, setUsername ] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        getUser(username);
+    }
+
+    const updateInput = e => {
+        const input = e.target.value 
+        setUsername(input)
+    }
+
     return (
         <nav>
-            <form className='searchBar'>
-                <input type="text"></input>
+            <form className='searchBar' onSubmit={handleSubmit}>
+                <input type="text" onChange={updateInput}></input>
                 <input type="submit" value="Search" />
             </form>
           <NavLink className="home-link" to="/">Home</NavLink>  
