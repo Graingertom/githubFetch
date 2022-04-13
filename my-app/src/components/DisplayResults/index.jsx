ffe8f2b87a5de05422ae4b2fd06e54422cd32f49
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-04-13 10:13:26 +0100

    adding keys to the item list

--- my-app/src/components/DisplayResults/index.jsx
+++ my-app/src/components/DisplayResults/index.jsx
@@ -6,26 +6,19 @@ import axios from 'axios';
 import { AiOutlineStar, AiOutlineFork } from 'react-icons/ai';
 import { VscIssues, VscGitPullRequest } from 'react-icons/vsc';
 import './style.css';
-import { useParams } from 'react-router-dom';
 
-export const DisplayResults = () => {
-
-    const {username} = useParams();
+export const DisplayResults = ({username}) => {
 
     const [userData, setUserData] = useState();
-    const [ statusMessage, setStatusMessage ] = useState('Loading');
 
     useEffect(() => {
     const userFetch = async () => {
-        setStatusMessage('Loading')
         try {
             let { data } = await axios.get(`https://api.github.com/users/${username}/repos`)
             console.log(data)
             setUserData(data)
-            setStatusMessage('')
         } catch (error) {
             console.log(error)
-            setStatusMessage(`Oops there\'s been an issue! ${error.message}`)
         }
     }
     userFetch()
@@ -36,9 +29,9 @@ export const DisplayResults = () => {
     return (
         <>
             <h1>{username}'s Repos</h1>
-            {
-                userData.map(user => (            
-            <div className='row'>
+            { userData &&
+                userData.map((user, index) => (            
+            <div key={index} className='row'>
                 <h3>{user.name}</h3>
                 <p>{user.description}</p>
                 <div className="icon">
